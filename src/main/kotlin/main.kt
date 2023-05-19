d11e5fdee5833bf671b6a933e3ed60f65562db2c
Artem Borokin <brutalgrin@gmail.com>
2023-05-19 15:32:15 +0300

    change main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,13 +4,13 @@ fun main() {
     println ("введите колличество лайков")
     var count:Int   = readLine()!!. toInt()
 
-    if (count == 11 || count % 100 == 20) {
+    if (count == 11 || count == 111 || count == 1311|| count == 10050011) {
         println("Понравилось $count людям")
     }
     else if (count == 1 || count % 10 == 1){
         println("Понравилось $count человеку")
     }
     else {
-        println("Понравилось $count людей")
+        println("Понравилось $count людям")
     }
 }
